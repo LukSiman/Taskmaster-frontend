2cfabfacb97917f3b67cc05c3d10cbf7a11ee242
LukSiman <simanavicius.lukas@gmail.com>
2022-10-13 10:29:46 -0700

    added day task component

---
+++ TaskmasterFE/src/app/day-tasks/day-tasks.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DayTasksComponent } from './day-tasks.component';
+
+describe('DayTasksComponent', () => {
+  let component: DayTasksComponent;
+  let fixture: ComponentFixture<DayTasksComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DayTasksComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DayTasksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ TaskmasterFE/src/app/day-tasks/day-tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day-tasks',
+  templateUrl: './day-tasks.component.html',
+  styleUrls: ['./day-tasks.component.scss']
+})
+export class DayTasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
