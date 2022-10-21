77dcfe08651f90a4632620c5b2c2b8796151b06e
LukSiman <simanavicius.lukas@gmail.com>
2022-10-20 17:29:26 -0700

    creating task object class

---
+++ TaskmasterFE/src/app/entities/task.ts
@@ -0,0 +1,11 @@
+export class Task {
+    taskUUID: string;
+    taskName: string;
+    taskOrder: number;
+    taskNote: string;
+    taskStatus: number;
+    taskStartTime: Date;
+    taskEndTime: Date;
+    taskDate: Date;
+    categoryName: string;
+}
