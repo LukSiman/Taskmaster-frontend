a689380b5696f97d6c70d01813b9039d90d206de
LukSiman <simanavicius.lukas@gmail.com>
2022-10-24 17:51:40 -0700

    added constructor

--- TaskmasterFE/src/app/entities/task.ts
+++ TaskmasterFE/src/app/entities/task.ts
@@ -8,4 +8,16 @@ export class Task {
     taskEndTime: Date;
     taskDate: Date;
     categoryName: string;
+
+    constructor() {
+        this.taskUUID = ""
+        this.taskName = "";
+        this.taskOrder = 0;
+        this.taskNote = "";
+        this.taskStatus = 0;
+        this.taskStartTime = new Date;
+        this.taskEndTime = new Date;
+        this.taskDate = new Date;
+        this.categoryName = "";
+    }
 }
