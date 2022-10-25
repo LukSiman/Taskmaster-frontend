adc896c1adff4c29054e19db41496ae014e5c63f
LukSiman <simanavicius.lukas@gmail.com>
2022-10-24 17:53:14 -0700

    added methods for getting tasks from service

--- TaskmasterFE/src/app/day-tasks/day-tasks.component.ts
+++ TaskmasterFE/src/app/day-tasks/day-tasks.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Task } from '../entities/task';
+import { TaskService } from '../services/task.service';
 
 @Component({
   selector: 'app-day-tasks',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day-tasks.component.scss']
 })
 export class DayTasksComponent implements OnInit {
+  //array to hold tasks
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(private taskService: TaskService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(() => {
+      this.displayTasks();
+    });
   }
 
+  //displays all task objects
+  displayTasks(): void {
+    this.taskService.getTasks().subscribe(response => {
+      this.tasks = response;
+      console.log(this.tasks);
+    });    
+  }
 }
