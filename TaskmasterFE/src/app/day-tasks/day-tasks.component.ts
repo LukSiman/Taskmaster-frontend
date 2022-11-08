15cb8d44a488c51a74625c6b82a1a202e7051d94
LukSiman <simanavicius.lukas@gmail.com>
2022-11-07 17:58:26 -0800

    added async calls

--- TaskmasterFE/src/app/day-tasks/day-tasks.component.ts
+++ TaskmasterFE/src/app/day-tasks/day-tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { firstValueFrom, lastValueFrom, Observable } from 'rxjs';
 import { Task } from '../entities/task';
 import { TaskService } from '../services/task.service';
 
@@ -16,12 +17,23 @@ export class DayTasksComponent implements OnInit {
   date: Date = new Date;
   today: string = "";
 
+  //current task
+  currentTask: Task = new Task;
+
   constructor(private taskService: TaskService, private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit(): void {
-    this.activatedRoute.paramMap.subscribe(() => {
-      this.displayCurrentDayTasks();
-    });
+  ngOnInit() {
+    // this.activatedRoute.paramMap.subscribe(() => {
+    //   this.displayCurrentDayTasks();
+    // });
+
+    this.start();
+  }
+
+  //starts the methods to get tasks
+  async start() {
+    await this.displayCurrentDayTasks()
+    this.findCurrentTask();
   }
 
   //displays all task objects
@@ -33,7 +45,7 @@ export class DayTasksComponent implements OnInit {
   }
 
   //displays tasks for the current day
-  displayCurrentDayTasks(): void {
+  async displayCurrentDayTasks(): Promise<void> {
     //update date each second
     setInterval(() => {
       this.date = new Date;
@@ -44,9 +56,31 @@ export class DayTasksComponent implements OnInit {
     this.today = `${this.date.getFullYear()}-${month}-${day}`;
 
     //send date to service for today's tasks
-    this.taskService.getTasksByDate(this.today).subscribe(response => {
-      this.tasks = response;
-      console.log(this.tasks);//TODO: DELETE
+    // this.taskService.getTasksByDate(this.today).subscribe(response => {
+    //   this.tasks = response;
+    //   console.log(this.tasks);//TODO: DELETE
+    // });
+
+    //send date to service for today's tasks
+    await lastValueFrom(this.taskService.getTasksByDate(this.today)).then(
+      response => {
+        this.tasks = response;
+        console.log(this.tasks);//TODO: DELETE
+      });
+
+    return new Promise<void>(resolve => {
+      resolve();
+    });
+  }
+
+  //find current task
+  findCurrentTask() {
+    console.log("Test");
+
+    this.tasks.forEach(task => {
+      console.log(`Start time: ${task.taskStartTime}`);
+      console.log(`End time: ${task.taskStartTime}`);
+      console.log(`Current time: ${this.date.getTime()}`);
     });
   }
 }
