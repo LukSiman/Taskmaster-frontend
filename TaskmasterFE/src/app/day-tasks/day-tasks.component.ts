5c0c9940509a93856f4e47116b21fa5fadb2ace3
Lukas Simanavicius <simanavicius.lukas@gmail.com>
2022-12-12 18:50:15 -0800

    adding methods for elapsed and remaining time

--- TaskmasterFE/src/app/day-tasks/day-tasks.component.ts
+++ TaskmasterFE/src/app/day-tasks/day-tasks.component.ts
@@ -19,8 +19,8 @@ export class DayTasksComponent implements OnInit {
 
   //time variables
   totalTaskTime: string = "";
-  elapsedTime: Date = new Date;
-  remainingTime: Date = new Date;
+  elapsedTime: string = "";
+  remainingTime: string = "";
 
   //task variables
   currentTask: Task | any = null;
@@ -51,17 +51,20 @@ export class DayTasksComponent implements OnInit {
     //update date each second
     setInterval(() => {
       this.date = new Date;
+
+      if (this.currentTask != null) {
+        let startTime: Date = this.getCorrectedDate(this.currentTask.taskStartTime);
+        let endTime: Date = this.getCorrectedDate(this.currentTask.taskEndTime);
+
+        this.elapsedTime = this.getTimeFormat(startTime, this.date);
+        this.remainingTime = this.getTimeFormat(this.date, endTime);
+      }
+
     }, 1000);
 
     let month: string = (this.date.getMonth() + 1).toString().padStart(2, "0");
     let day: string = this.date.getDate().toString().padStart(2, "0");
     this.today = `${this.date.getFullYear()}-${month}-${day}`;
-
-    //send date to service for today's tasks
-    // this.taskService.getTasksByDate(this.today).subscribe(response => {
-    //   this.tasks = response;
-    //   console.log(this.tasks);//TODO: DELETE
-    // });
 
     //send date to service for today's tasks
     await lastValueFrom(this.taskService.getTasksByDate(this.today)).then(
@@ -87,25 +90,15 @@ export class DayTasksComponent implements OnInit {
       }
 
       //get start time of the task
-      let startTime = new Date();
-      startTime.setHours(task.taskStartTime.toString().split(",").map(Number)[0]);
-      startTime.setMinutes(task.taskStartTime.toString().split(",").map(Number)[1]);
-      startTime.setSeconds(0);
+      let startTime: Date = this.getCorrectedDate(task.taskStartTime);
 
       //get end time if the task
-      let endTime = new Date();
-      endTime.setHours(task.taskEndTime.toString().split(",").map(Number)[0]);
-      endTime.setMinutes(task.taskEndTime.toString().split(",").map(Number)[1]);
-      endTime.setSeconds(0);
+      let endTime: Date = this.getCorrectedDate(task.taskEndTime);
 
       //set current task if it aligns with current time
       if (startTime < this.date && endTime > this.date) {
         this.currentTask = task;
-        let differenceInSeconds: number = (endTime.getTime() - startTime.getTime()) / 1000;
-        let seconds: number = differenceInSeconds % 60;
-        let minutes: number = (differenceInSeconds / 60) % 60;
-        let hours: number = Math.trunc((differenceInSeconds / 60 / 60) % 24);
-        this.totalTaskTime = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+        this.totalTaskTime = this.getTimeFormat(startTime, endTime);
 
         return task;
       }
@@ -113,5 +106,25 @@ export class DayTasksComponent implements OnInit {
     });
   }
 
+  // returns task time in correct format
+  getCorrectedDate(time: Date): Date {
+    let returnTime = new Date();
+    returnTime.setHours(time.toString().split(",").map(Number)[0]);
+    returnTime.setMinutes(time.toString().split(",").map(Number)[1]);
+    returnTime.setSeconds(0);
+
+    return returnTime;
+  }
+
+  // returns a string in the correct time format
+  getTimeFormat(startTime: Date, endTime: Date): string {
+    let differenceInSeconds: number = (endTime.getTime() - startTime.getTime()) / 1000;
+    let seconds: number = differenceInSeconds % 60;
+    let minutes: number = (differenceInSeconds / 60) % 60;
+    let hours: number = Math.trunc((differenceInSeconds / 60 / 60) % 24);
+    let formattedString = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+
+    return formattedString;
+  }
 
 }
